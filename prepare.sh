c39fbc3c916cbff2d296ce7a362fd61fe0f6733e
Steve Göring <steve.goering@tu-ilmenau.de>
2023-04-27 14:29:35 +0200

    changes

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://ftp.tu-ilmenau.de/hpc-private/ei/avt/sophoappeal/rule_prediction_extension_models.zip"
+wget "https://resdata.tu-ilmenau.de/public/ei/avt/sophoappeal/rule_prediction_extension_models.zip"
 # extract
 unzip rule_prediction_extension_models.zip
 
